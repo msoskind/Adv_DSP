e704c2684f0197ac2a845ed16d12484377202563
kiennguyen94 <hakien94@gmail.com>
2017-05-10 13:26:26 -0400

    update train_rcnn

--- train_rcnn.m
+++ train_rcnn.m
@@ -18,6 +18,8 @@ data = textscan(fileID, '%s %f %f %f %f %f %f');
 %%
 % Making the new pupil/iris metadata data (for R-CNN)
 imageFilenames = data{1};
+% imageFilenames = strcat(repmat({'resized/'}, [54,1]),data{1});
+
 for i = 1:length(data{1})
     pupils{i} = [data{2}(i)-data{4}(i), data{3}(i)-data{4}(i), 2*data{4}(i), 2*data{4}(i)];
     irises{i} = [data{5}(i)-data{7}(i), data{6}(i)-data{7}(i), 2*data{7}(i), 2*data{7}(i)];
@@ -58,6 +60,26 @@ layers1 = [
     softmaxLayer()
     classificationLayer()
 ];
+layers2 = [
+    imageInputLayer([250 250 3])
+    convolution2dLayer(5,32)
+    maxPooling2dLayer(3, 'Stride', 2)
+    reluLayer()
+    
+    convolution2dLayer(5,32)
+    reluLayer()
+    averagePooling2dLayer(3, 'Stride', 2)
+    
+    convolution2dLayer(5,64)
+    reluLayer()
+    averagePooling2dLayer(3, 'Stride', 2)
+    
+    fullyConnectedLayer(64)
+    reluLayer()
+    fullyConnectedLayer(numClassesPlusBackground)
+    softmaxLayer()
+    classificationLayer()
+];
 % load('rcnnStopSigns.mat', 'stopSigns', 'layers')
 % myLayers = [
 %     imageInputLayer([250,250,3])
@@ -70,20 +92,24 @@ layers1 = [
 % pre-trained network and prevents the network from changing too rapidly. 
 options = trainingOptions('sgdm', ...
   'MiniBatchSize', 16, ...
-  'InitialLearnRate', 1e-6, ...
-  'MaxEpochs', 10);
+  'InitialLearnRate', 1e-4, ...
+  'MaxEpochs', 20);
 
 %%
 % Train the R-CNN detector. Training can take a few minutes to complete.
-rcnn_p = trainRCNNObjectDetector(table_p, layers1, options, 'NegativeOverlapRange', [0 0.3]);
+gpuDevice(1);
+rcnn_p = trainRCNNObjectDetector(table_p, layers2, options, 'NegativeOverlapRange', [0 0.3]);
+% save ('rcnn_p.mat', rcnn_p)
 
 %%
 % Test the R-CNN detector on a test image.
-img = imread('test_eye.jpg'); 
-% img = imread('eye_01.jpg');
-img_diff = imresize(img, [250 250]);
+img = imread('cropped/test_eye.jpg'); 
+% img = imread('resized/eye_05.jpg');
+img_diff = imresize(img, [250 NaN]);
+% img_d = gpuArray(img_diff);
+% img_d = im2uint8(img_d);
 
-[bbox, score, label] = detect(rcnn_p, img, 'MiniBatchSize', 16);
+[bbox, score, label] = detect(rcnn_p, img, 'MiniBatchSize',1);
 %%
 % Display strongest detection result.
 [score, idx] = max(score);
